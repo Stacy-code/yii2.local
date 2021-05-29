969ce0cac2de6705d702d24aeb9fc62f4bae5563
Stacy-code <64099898+Stacy-code@users.noreply.github.com>
2021-05-29 17:24:12 +0300

    add admin themes

--- modules/admin/assets/AdminAsset.php
+++ modules/admin/assets/AdminAsset.php
@@ -12,14 +12,17 @@ class AdminAsset extends BaseAssetBundle
      * @var string $sourcePath
      * Папка со стилями и скриптами
      */
-    public $sourcePath = '@webroot/themes/frontend';
+    public $sourcePath = '@webroot/themes/backend';
     /**
      * @var array
      * Підключаємо стилі
      */
     public $css = [
-        'https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900%7COpen+Sans:300,300i,400,400i,600,600i,700,700i,800,800i',
-        'css/external.css', 'css/bootstrap.min.css', 'css/style.css'
+        'css/app.min.css', 'css/icons.min.css', 'css/bootstrap.min.css',
+        'libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css',
+        'libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css',
+        'libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css',
+        'images/favicon.ico'
     ];
 
     /**
@@ -27,7 +30,20 @@ class AdminAsset extends BaseAssetBundle
      * Підключаємо скріпти
      */
     public $js = [
-        'js/plugins.js', 'js/functions.js'
+        'js/pages/dashboard.init.js',
+        'js/app.js',
+        'libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js',
+        'libs/datatables.net-responsive/js/dataTables.responsive.min.js',
+        'libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js',
+        'libs/datatables.net/js/jquery.dataTables.min.js',
+        'libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js',
+        'libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js',
+        'libs/apexcharts/apexcharts.min.js',
+        'libs/node-waves/waves.min.js',
+        'libs/simplebar/simplebar.min.js',
+        'libs/metismenu/metisMenu.min.js',
+        'libs/bootstrap/js/bootstrap.bundle.min.js',
+        'libs/jquery/jquery.min.js'
     ];
 
     /**
