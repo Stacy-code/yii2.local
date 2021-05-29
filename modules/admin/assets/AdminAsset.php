38aa27fbc84d9af6b7260a3fcf424fa487028656
Stacy-code <64099898+Stacy-code@users.noreply.github.com>
2021-05-29 18:38:16 +0300

    menu

--- modules/admin/assets/AdminAsset.php
+++ modules/admin/assets/AdminAsset.php
@@ -19,9 +19,9 @@ class AdminAsset extends BaseAssetBundle
      */
     public $css = [
         'css/app.min.css', 'css/icons.min.css', 'css/bootstrap.min.css',
-        'libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css',
-        'libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css',
-        'libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css',
+      //  'libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css',
+      //  'libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css',
+      //  'libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css',
         'images/favicon.ico'
     ];
 
@@ -32,18 +32,18 @@ class AdminAsset extends BaseAssetBundle
     public $js = [
         'js/pages/dashboard.init.js',
         'js/app.js',
-        'libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js',
-        'libs/datatables.net-responsive/js/dataTables.responsive.min.js',
-        'libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js',
-        'libs/datatables.net/js/jquery.dataTables.min.js',
-        'libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js',
-        'libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js',
-        'libs/apexcharts/apexcharts.min.js',
-        'libs/node-waves/waves.min.js',
-        'libs/simplebar/simplebar.min.js',
-        'libs/metismenu/metisMenu.min.js',
-        'libs/bootstrap/js/bootstrap.bundle.min.js',
-        'libs/jquery/jquery.min.js'
+        //'libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js',
+      //  'libs/datatables.net-responsive/js/dataTables.responsive.min.js',
+      //  'libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js',
+     //   'libs/datatables.net/js/jquery.dataTables.min.js',
+       // 'libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js',
+    //    'libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js',
+    //    'libs/apexcharts/apexcharts.min.js',
+      //  'libs/node-waves/waves.min.js',
+     //   'libs/simplebar/simplebar.min.js',
+      //  'libs/metismenu/metisMenu.min.js',
+   //     'libs/bootstrap/js/bootstrap.bundle.min.js',
+   //     'libs/jquery/jquery.min.js'
     ];
 
     /**
